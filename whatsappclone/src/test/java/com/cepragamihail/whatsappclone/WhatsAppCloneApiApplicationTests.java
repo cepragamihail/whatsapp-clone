3b87b5054fd117e8bef3da8e31338d4c17225b0b
Mihail Cepraga <yahim09@gmail.com>
2025-06-19 14:16:27 +0300

    init API

---
+++ whatsappclone/src/test/java/com/cepragamihail/whatsappclone/WhatsAppCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cepragamihail.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
